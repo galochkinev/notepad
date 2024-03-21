5d57f72d43104a0e00a0373939924c532c0b1644
Nikita <157800165+NikitaTumanov765@users.noreply.github.com>
2024-03-21 10:14:21 +0300

    Fixed #3

---
+++ singletons/SIN_FILE.gd
@@ -0,0 +1,31 @@
+extends Node
+
+var work_path: String = 'user://data.dat'
+var notes: Array = []
+
+func _ready():
+	if data_exists():
+		data_load()
+	else:
+		data_save()
+
+func data_load():
+	var filer = FileAccess.open(work_path, FileAccess.READ)
+	notes = filer.get_var()
+	filer.close()
+
+func data_save():
+	var filer = FileAccess.open(work_path, FileAccess.WRITE)
+	filer.store_var(notes)
+	filer.close()
+	
+func data_exists():
+	return FileAccess.file_exists(work_path)
+
+func add_note(code: String, text: String):
+	var note: Dictionary = {
+		'code': code,
+		'text': text
+	}
+	notes.append(note)
+	data_save()
--- windows/main/main.gd
+++ windows/main/main.gd
@@ -1,17 +1,19 @@
 extends Control
 
 @onready var text = $bg/text
+@onready var panel_save_note = $bg/panel_save_note
+var isDialogSaveOpened: bool = false
 
 func _ready():
-	pass # Replace with function body.
+	panel_save_note.visible = false
 
 
 func _on_записи_id_pressed(id):
-	print(id)
 	match id:
 		0:
 			# SAVE
-			pass
+			isDialogSaveOpened = not isDialogSaveOpened
+			panel_save_note.visible = isDialogSaveOpened
 		1:
 			# OPEN
 			get_tree().change_scene_to_file("res://windows/search/search.tscn")
@@ -22,3 +24,13 @@ func _on_записи_id_pressed(id):
 
 func _on_выход_about_to_popup():
 	get_tree().quit()
+
+
+func _on_cancel_pressed():
+	panel_save_note.visible = false
+
+
+func _on_save_pressed():
+	SIN_FILE.add_note($bg/panel_save_note/code.text, $bg/text.text)
+	panel_save_note.visible = false
+	
